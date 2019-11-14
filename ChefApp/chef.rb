38eab5a97974b3bdfe94028f66d2278663b917b3
Ricky Arora <rtyer3@gmail.com>
2019-11-14 14:53:24 -0500

    removing extra end

--- ChefApp/chef.rb
+++ ChefApp/chef.rb
@@ -2,65 +2,64 @@ require_relative 'dish'
 require 'byebug'
 class Chef
   attr_accessor :current_dishes
-      def initialize(dishes=[])
-        @current_dishes = dishes
-      end
-
-      def display_menu
-        puts "Type: \n 'a' to add a dish \n 'r' to remove a dish \n 'd' to display all dishes \n 'q' to quit the program"
-      end
-
-      def get_input
+  def initialize(dishes=[])
+    @current_dishes = dishes
+  end
+  
+  def display_menu
+    puts "Type: \n 'a' to add a dish \n 'r' to remove a dish \n 'd' to display all dishes \n 'q' to quit the program"
+  end
+  
+  def get_input
+    display_menu
+    input = ""
+    while(input != 'q')
+      input = gets.chomp
+      parse_input(input)
+      if input != 'q'
         display_menu
-        input = ""
-        while(input != 'q')
-          input = gets.chomp
-          parse_input(input)
-          if input != 'q'
-            display_menu
-          end
-        end
       end
+    end
+  end
+  
+  def parse_input(input) 
+    if input == 'a'
+      add
+    elsif input == 'r'
+      remove
+    elsif input == 'd'
+      current_dishes_list
+    elsif input == 'q'
+      return 
+    else puts "invalid input please pick a valid option"
+    end
+  end
+  
+  def add
+    puts "input the name of your new dish \n"
+    dish_to_add = gets.chomp
+    @current_dishes.push(Dish.new(dish_to_add))
+    puts "dish added: " + dish_to_add
+  end
 
-      def parse_input(input) 
-        if input == 'a'
-          add
-        elsif input == 'r'
-          remove
-        elsif input == 'd'
-          current_dishes_list
-        elsif input == 'q'
-          return 
-        else puts "invalid input please pick a valid option"
-        end
-      end
+def remove
+  puts "input the name of the dish you wish to remove \n"
+  name_of_dish_to_remove = gets.chomp
+  array_with_dish_deleted = @current_dishes.reject! { |dish| dish.to_s == name_of_dish_to_remove }
+  if array_with_dish_deleted.nil?
+    puts "dish did not exist, so no dish removed"
+  else
+    puts "dish removed: " + name_of_dish_to_remove
+  end
+end
 
-      def add
-        puts "input the name of your new dish \n"
-        dish_to_add = gets.chomp
-          @current_dishes.push(Dish.new(dish_to_add))
-            puts "dish added: " + dish_to_add
-        end
-      end
-
-      def remove
-        puts "input the name of the dish you wish to remove \n"
-        name_of_dish_to_remove = gets.chomp
-        array_with_dish_deleted = @current_dishes.reject! { |dish| dish.to_s == name_of_dish_to_remove }
-        if array_with_dish_deleted.nil?
-          puts "dish did not exist, so no dish removed"
-        else
-          puts "dish removed: " + name_of_dish_to_remove
-        end
-      end
-
-      def current_dishes_list
-        puts "list of dishes displayed below: \n \n"
-        @current_dishes.each do |dish| 
-          puts dish.name
-        end
-        puts "\nYour list of food is displayed above."
-      end
+def current_dishes_list
+  puts "list of dishes displayed below: \n \n"
+  @current_dishes.each do |dish| 
+    puts dish.name
+  end
+  puts "\nYour list of food is displayed above."
+end
 end
 
 
